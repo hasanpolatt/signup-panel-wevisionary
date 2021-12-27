5e0272c23e674c14baeeff8a1b3a7266e5817e8a
unknown <thatsnotpublic@gmail.com>
2021-12-28 01:12:28 +0300

    project

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('Hello Everyone'));
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
+
