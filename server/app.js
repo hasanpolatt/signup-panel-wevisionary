4a9e341dfc1c7ee206183c7ff82cd81957b2ea04
unknown <thatsnotpublic@gmail.com>
2021-12-28 20:43:05 +0300

    feat: crud system on api

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded( bodyparser.urlencoded({ extended: true }) ));
+
+const db = require('./queries');
+
 app.get('/', (req, res) => res.send('Hello Everyone'));
+app.get('/users', db.getUsers);
+app.get('/user/:id', db.getUserById);
+app.post('/user', db.createUser)
+app.put('/user/:id', db.updateUser)
+app.delete('/user/:id', db.deleteUser)
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
+
+
+
+
 
